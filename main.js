de8e99fc5d36ff0d7282ab52e9e7f60bae5d6e26
John Schimenz <john.schimenz@gmail.com>
2024-01-14 09:23:00 -0600

    Set up dialog elements for form in HTML, but functionality does not work

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-console.log('js works');
+// List of all document.querySelector's
+const clickOpenDialogButton = document.querySelector('#open-dialog');
+const selectDialogForm = document.querySelector('#modal');
+
+clickOpenDialogButton.addEventListener('click', () => {
+    
+})
