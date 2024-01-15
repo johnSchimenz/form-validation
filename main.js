d8c46a7bb33f933bf1bd566f6848f3334959017c
John Schimenz <john.schimenz@gmail.com>
2024-01-15 16:04:28 -0600

    Shows modal where type in information when button is clicked

--- main.js
+++ main.js
@@ -2,6 +2,12 @@
 const clickOpenDialogButton = document.querySelector('#open-dialog');
 const selectDialogForm = document.querySelector('#modal');
 
-clickOpenDialogButton.addEventListener('click', () => {
-    
+clickOpenDialogButton.addEventListener('click', (event) => {
+
+    // Prevents form from being submitted
+    event.preventDefault();
+
+    // Opens up modal from dialog element
+    selectDialogForm.showModal();
+
 })
